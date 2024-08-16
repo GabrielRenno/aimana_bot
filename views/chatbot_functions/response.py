298d5bc63ec03fe390179670b711eb80f780cdbc
GabrielRenno <gabriel_renno@outlook.com>
2024-08-16 14:00:00 -0500

    commit

--- views/chatbot_functions/response.py
+++ views/chatbot_functions/response.py
@@ -2,7 +2,4 @@ import random
 import time
 def response_generator(prompt):
     response = prompt
-    return response
-    for word in response.split():
-        yield word + " "
-        time.sleep(0.05)
+    return str(response)

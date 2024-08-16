94122e625fe9636ea3bebb2ec70be71fe8659463
GabrielRenno <gabriel_renno@outlook.com>
2024-08-16 12:20:38 -0500

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,14 @@ if authentication_status == True:
     st.logo("assets/logo.jpeg")
     auth.logout(":material/logout:",'sidebar')
     st.sidebar.text("Developed by Aimana")
-
+    hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_st_style, unsafe_allow_html=True)
 
     # --- RUN NAVIGATION ---
     pg.run()

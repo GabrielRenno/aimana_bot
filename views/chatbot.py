215acb1f88f5fb60b793fe7398e33ea6e9721e64
GabrielRenno <gabriel_renno@outlook.com>
2024-08-16 13:39:25 -0500

    random

--- views/chatbot.py
+++ views/chatbot.py
@@ -1,21 +1,5 @@
 import streamlit as st
-import random
-import time
-
-
-# Streamed response emulator
-def response_generator():
-    response = random.choice(
-        [
-            "Hello there! How can I assist you today?",
-            "Hi, human! Is there anything I can help you with?",
-            "Do you need help?",
-        ]
-    )
-    for word in response.split():
-        yield word + " "
-        time.sleep(0.05)
-
+from views.chatbot_functions.response import response_generator
 
 st.title("Simple chat")
 

e637dac7bf9cf10f530b2d8de8a2d6332f5ad348
GabrielRenno <gabriel_renno@outlook.com>
2024-08-16 13:49:54 -0500

    commit

--- views/chatbot.py
+++ views/chatbot.py
@@ -8,13 +8,13 @@ st.title("Simple chat")
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        # Define the avatar based on the role
-        avatar = ":material/smart_toy:" if message["role"] == "assistant" else ":material/person:"
-        
-        with st.chat_message(message["role"], avatar=avatar):
-            st.markdown(message["content"])
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    # Define the avatar based on the role
+    avatar = ":material/smart_toy:" if message["role"] == "assistant" else ":material/person:"
+    
+    with st.chat_message(message["role"], avatar=avatar):
+        st.markdown(message["content"])
 
 # Accept user input
 if prompt := st.chat_input("What is up?"):
@@ -32,9 +32,9 @@ if prompt := st.chat_input("What is up?"):
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
 
-
 # Restart chat button
-def reset_conversation():
-  st.session_state.messages = None
-st.button('Reset Chat', on_click=reset_conversation)
+if st.button("Restart Chat"):
+    st.session_state.messages = []
+    # Stop displaying messages
+    st.stop()
     
